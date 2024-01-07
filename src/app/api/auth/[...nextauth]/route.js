8f84772b322a0f895bbd65cc70b7b85b82f1c5b3
yeferson <yefersonapk@gmail.com>
2024-01-06 23:54:54 -0500

    one

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import db from '@/libs/db'
+import bcrypt from 'bcrypt'
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password", placeholder: "*****" },
+      },
+      async authorize(credentials, req) {
+        console.log(credentials)
+
+        const userFound = await db.user.findUnique({
+            where: {
+                email: credentials.email
+            }
+        })
+
+        console.log("vamos con todo")
+        if (!userFound) return null
+
+
+        console.log(userFound)
+
+        const matchPassword = await bcrypt.compare(credentials.password, userFound.password)
+
+        if (!matchPassword) return null
+
+        return {
+            id: userFound.id,
+            name: userFound.username,
+            email: userFound.email,
+        }
+      },
+    }),
+  ],
+//   pages: {
+//     signIn: "/auth/login",
+//   }
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='bg-red-500'>Login</div>
+  )
+}
+
+export default Login
